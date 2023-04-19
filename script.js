b51a78d6b251831d2baa6662d16e267325f93aa5
Miguel-klk <124157850+Miguel-klk@users.noreply.github.com>
2023-04-19 13:23:32 +0200

    guardar

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var nombre = prompt("¿Como te llams?:");
+var nombre = prompt("Por favor, pon tu nombre aquí:");
+
 nombre = nombre.toLowerCase().replace(/\s+/g, '');
+
 var nombreAlReves = '';
+
 for (var i = nombre.length - 1; i >= 0; i--) {
-    nombreAlReves += nombre[i];}
+    nombreAlReves += nombre[i];
+}
+
 alert("¡Adiós " + nombreAlReves + "!");
