7d7b886252ea909efdce23728cdb141cc3f097a5
Miguel-klk <124157850+Miguel-klk@users.noreply.github.com>
2023-04-19 13:17:53 +0200

    Actividad 5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let nombre = prompt("¿Como te llamas?");
 let nombreReverso = "";
 for (let i = nombre.length - 1; i >= 0; i--) {nombreReverso += nombre[i];}
+
 console.log("¡Adiós " + nombreReverso + "!");
